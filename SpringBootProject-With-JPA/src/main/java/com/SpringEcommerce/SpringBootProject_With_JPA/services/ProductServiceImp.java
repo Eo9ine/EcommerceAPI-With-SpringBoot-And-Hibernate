a9087b053a82c0105a102bebc88d5eb636ebb642
proneon <pntombelafirst@gmail.com>
2025-04-10 20:11:13 +0200

    Added the service annotation to serviceImplentation

--- SpringBootProject-With-JPA/src/main/java/com/SpringEcommerce/SpringBootProject_With_JPA/services/ProductServiceImp.java
+++ SpringBootProject-With-JPA/src/main/java/com/SpringEcommerce/SpringBootProject_With_JPA/services/ProductServiceImp.java
@@ -1,4 +1,7 @@
 package com.SpringEcommerce.SpringBootProject_With_JPA.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductServiceImp {
 }
--- SpringBootProject-With-JPA/src/main/java/com/SpringEcommerce/SpringBootProject_With_JPA/services/ProductServiceInterface.java
+++ SpringBootProject-With-JPA/src/main/java/com/SpringEcommerce/SpringBootProject_With_JPA/services/ProductServiceInterface.java
@@ -1,4 +1,7 @@
 package com.SpringEcommerce.SpringBootProject_With_JPA.services;
 
+import org.springframework.stereotype.Service;
+
+
 public interface ProductServiceInterface {
 }
